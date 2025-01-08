4b94efef079de8c7348bf87d81b336512b0c6e76
Michael Kennedy <michael@michaelkennedy.dev>
2025-01-07 23:06:09 -0800

    Cleaning up

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ import { render } from 'resumed'
 const resume = JSON.parse(await fs.readFile('resume.json', 'utf-8'))
 const html = await render(resume, theme)
 
+
+
 const browser = await puppeteer.launch()
 const page = await browser.newPage()
 
 await page.setContent(html, { waitUntil: 'networkidle0' })
 await page.addStyleTag({path: "theme/stylesheet.css"})
-await page.pdf({ path: `${resume.basics.name.replace(" ", '')}_Resume.pdf`, format: 'Letter', printBackground: false })
+await page.pdf({ path: `${resume.basics.name.replace(" ", '')}_Resume.pdf`, format: 'Letter', printBackground: false, tagged: true })
 await browser.close()
--- theme/index.js
+++ theme/index.js
@@ -11,25 +11,30 @@ exports.render = ({ basics, work, education, skills, projects }) => `
   <div id="heading">
     <h1>${basics.name}</h1>
     <ol id="contact">
-      <li>${basics.phone}</li>
+      <li><a href="tel:${basics.phone}" aria-label=${basics.phone.replace("(", "").replace(")", "-").replace(" ", "")}>${basics.phone}</a></li>
       <li><a href="mailto:${basics.email}">${basics.email}</a></li>
       ${basics.profiles.map((profile) => `<li><a href="${profile.url}">${profile.display_name}</a></li>`).join("")}
     </ol>
   </div>
-  ${basics?.summary && `
+  ${
+    basics.summary &&
+    `
     <section id="summary-section" class="main-section">
       <h2>SUMMARY</h2>
       <p>${basics.summary}</p>
   </section>
-    `}
-  ${work && `
+    `
+  }
+  ${
+    work &&
+    `
     <section id="experience-section" class="main-section">
     <h2>EXPERIENCE</h2>
-    <ol>
+    <ol id="experience-list">
       ${work
         .map(
           (exp) =>
-            `<li class="experience-item">
+            `<li>
                 <div class="section-header">
                   <div class="header-1">
                     <h3>${exp.name}</h3>
@@ -48,58 +53,73 @@ exports.render = ({ basics, work, education, skills, projects }) => `
         .join("")}
     </ol>
     </section>
-    `}
-    ${skills && `    
+    `
+  }
+    ${
+      skills &&
+      `    
       <section id="technical-skills-section" class="main-section">
         <h2>TECHNICAL SKILLS</h2>
-          <ol id="technical-skills">
-          ${skills.map(skill => `<li><b>${skill.name}</b>: ${skill.highlights.join(", ")}</li>`).join("")}
+          <ol id="technical-skills-list">
+          ${skills.map((skill) => `<li><b>${skill.name}</b>: ${skill.highlights.join(", ")}</li>`).join("")}
           </ol>
-    </section>`}
-    ${projects && `
+    </section>`
+    }
+    ${
+      projects &&
+      `
       <section id="projects-section" class="main-section">
         <h2>PROJECTS</h2>
-          <ol>
+          <ol id="project-list">
             ${projects
               .map(
                 (project) =>
                   `<li>
                 <div class="section-header">
-                <div class="header-1">
-                <h3>${project.name}</h3>
-                ${project?.description ? `<p>${project.description}</p>` : ''}
-                ${project.technologies && `<p><i>${project.technologies.map(tech => `${tech}`).join(', ')}</i></p>`}
-              </div>
-              <div class='header-2'>
-                <span>${project.startDate} - ${project?.endDate ?? "Present"}</span>
-              </div>
-              </div>
-              <ol class="highlights">
-                    ${project.highlights.map((hi) => `<li>${hi}</li>`).join("")}
-              </ol>
+                  <div class="header-1">
+                  <h3>${project.name}</h3>
+                  ${project?.description ? `<p>${project.description}</p>` : ""}
+                  ${project.technologies && `<p><i>${project.technologies.map((tech) => `${tech}`).join(", ")}</i></p>`}
+                </div>
+                <div class='header-2'>
+                  <span>${project.startDate} - ${project?.endDate ?? "Present"}</span>
+                </div>
+                </div>
+                <ol class="highlights">
+                      ${project.highlights.map((hi) => `<li>${hi}</li>`).join("")}
+                </ol>
             </li>`
               )
               .join("")}
           </ol>
-    </section>`}
-   ${education && ` 
+    </section>`
+    }
+   ${
+     education &&
+     ` 
     <section id="education-section" class="main-section">
     <h2>EDUCATION</h2>
-    <ol>
-    ${education.map(edu => `<li>
+    <ol id="education-list">
+    ${education
+      .map(
+        (edu) => `<li>
       <div class="section-header">
         <div class="header-1">
           <h3>${edu.institution}</h3>
-          <span>${edu.studyType} in ${edu.area} ${edu.minor && `, Minor in ${edu.minor}`}</span>
+          <p>${edu.studyType} in ${edu.area}${edu.minor && `, Minor in ${edu.minor}`}</p>
         </div>
         <div class="header-2">
           <span>${edu.startDate} - ${edu?.endDate ?? "Present"}</span>
           <p>${edu.location}</p>
         <div>
       </div>
-      </li>`).join("")}
+      </li>`
+      )
+      .join("")}
     </ol>
-    </section>`}
+    </section>`
+   }
     </article>
   </body>
 </html>`;
+
